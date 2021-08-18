c21fc109956c2be57afbd262bc8fbfa43e2e3974
yer2018 <42699536+yer2018@users.noreply.github.com>
2021-08-18 17:20:32 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.254/g' package/base-files/files/bin/config_generate
+sed -i s/luci-theme-bootstrap/luci-theme-argon/ feeds/luci/collections/luci/Makefile 
