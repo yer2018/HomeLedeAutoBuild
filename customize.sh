6809a95e28ecce80a02ed15e2312f26b5e277da6
yer2018 <42699536+yer2018@users.noreply.github.com>
2021-08-19 00:07:36 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,3 +2,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.254/g' package/base-files/files/bin/config_generate
 sed -i s/luci-theme-bootstrap/luci-theme-argon/ feeds/luci/collections/luci/Makefile 
+sed -i 's/OpenWrt/HomeLEDE/g' package/base-files/files/bin/config_generate
