05aee719d60ead0060a78f8c0b632f2830a4a4f4
yer2018 <42699536+yer2018@users.noreply.github.com>
2021-09-14 18:03:05 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.254/g' package/base-files/files/bin/config_generate
+# Modify default Theme
 sed -i s/luci-theme-bootstrap/luci-theme-argon/ feeds/luci/collections/luci/Makefile 
+# Modify default HostName
 sed -i 's/OpenWrt/HomeLEDE/g' package/base-files/files/bin/config_generate
+# Modify default PassWord
+sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$a87b3JDA$O5S5vtQFGIL9deGI2KeBg1:0:0:99999:7:::/g' /etc/shadow
