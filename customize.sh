bb343bf36bc907dcbdfbb1e2edfa4d90a6075338
yer2018 <42699536+yer2018@users.noreply.github.com>
2021-11-28 23:24:51 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -11,7 +11,7 @@ sed -i 's/OpenWrt/HomeLEDE/g' package/base-files/files/bin/config_generate
 # Modify default PassWord
 sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$a87b3JDA$O5S5vtQFGIL9deGI2KeBg1:0:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
 # Add ssh-rsa
-sed -i '33a echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDs1m4Ts+ZDF0qEu0kNypyQ//lJFIeAGNMBtCO9KzNKHKxPWnPrc7w1Eu43HqBZGH2VDmX/gOF+7xu8H49g+xMHHsSO/OjnQlYZoYDMRwAiceScQiEa6NBxJZ9upgdrPaSOBJ1LWPJDY9JbIqXYLJCCps3ZVxkw4p1PgLLTkTNvsx/KRbLQzuLsHtGHqcRh5VEqAqwiXnRyqVhoT8EGarbwO/jfSIWYWAskyISBbly4Otq9pwl+UCLCDq8h+0stUlvhRWsbI0+O24+w9c6ldGT/gBn7rQiujjCVEPSZTe7+8JkPiMQSzurnSNpCvF5dgHNw9uXdTOIvVNBzOGc+e09nTWJqkYUQDMebujQH4ZxZkGQx4lrMtofmJyFQV+kkhzMASX8OS9jTD/gJSmBUWOODc2rvbE6E2iSBTRmHWIkwmSz31PDktocf1OtPthsIbnxuCaK3FuTDnSe0lJROhoawF9B+s9+vp8+n2ORrqFsyrtay9v7FNCiS7jMXdjX7wr8= ye@MacBook-Pro" > /etc/dropbear/authorized_keys' package/lean/default-settings/files/zzz-default-settings
+sed -i '40a echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDs1m4Ts+ZDF0qEu0kNypyQ//lJFIeAGNMBtCO9KzNKHKxPWnPrc7w1Eu43HqBZGH2VDmX/gOF+7xu8H49g+xMHHsSO/OjnQlYZoYDMRwAiceScQiEa6NBxJZ9upgdrPaSOBJ1LWPJDY9JbIqXYLJCCps3ZVxkw4p1PgLLTkTNvsx/KRbLQzuLsHtGHqcRh5VEqAqwiXnRyqVhoT8EGarbwO/jfSIWYWAskyISBbly4Otq9pwl+UCLCDq8h+0stUlvhRWsbI0+O24+w9c6ldGT/gBn7rQiujjCVEPSZTe7+8JkPiMQSzurnSNpCvF5dgHNw9uXdTOIvVNBzOGc+e09nTWJqkYUQDMebujQH4ZxZkGQx4lrMtofmJyFQV+kkhzMASX8OS9jTD/gJSmBUWOODc2rvbE6E2iSBTRmHWIkwmSz31PDktocf1OtPthsIbnxuCaK3FuTDnSe0lJROhoawF9B+s9+vp8+n2ORrqFsyrtay9v7FNCiS7jMXdjX7wr8= ye@MacBook-Pro" > /etc/dropbear/authorized_keys' package/lean/default-settings/files/zzz-default-settings
 # Modify default Shell
 #sed -i '43a sed -i "s/\\\/bin\\\/ash/\\\/usr\\\/bin\\\/zsh/g" /etc/passwd' package/lean/default-settings/files/zzz-default-settings
 # Modify build date
@@ -24,11 +24,17 @@ sed -i '33a echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDs1m4Ts+ZDF0qEu0kNypyQ//
 #sed -i '34a sed -i "s/<%=ping_host%>/www.baidu.com/g" /usr/lib/lua/luci/view/admin_network/diagnostics.htm' package/lean/default-settings/files/zzz-default-settings
 #sed -i '35a sed -i "s/<%=route_host%>/www.baidu.com/g" /usr/lib/lua/luci/view/admin_network/diagnostics.htm' package/lean/default-settings/files/zzz-default-settings
 #sed -i '36a sed -i "s/<%=dns_host%>/www.baidu.com/g" /usr/lib/lua/luci/view/admin_network/diagnostics.htm' package/lean/default-settings/files/zzz-default-settings
-sed -i '32a sed -i "s/openwrt.org/www.baidu.com/g" /etc/config/luci'  package/lean/default-settings/files/zzz-default-settings
+#sed -i '32a sed -i "s/openwrt.org/www.baidu.com/g" /etc/config/luci'  package/lean/default-settings/files/zzz-default-settings
 # Disable
 #sed -i '34a cp /etc/rc.local /etc/rc.local.bak' package/lean/default-settings/files/zzz-default-settings
-sed -i '35a sed -i "2a /etc/init.d/odhcpd disable" /etc/rc.local' package/lean/default-settings/files/zzz-default-settings
-sed -i '36a sed -i "3a /etc/init.d/led disable" /etc/rc.local' package/lean/default-settings/files/zzz-default-settings
+#sed -i '35a sed -i "2a /etc/init.d/odhcpd disable" /etc/rc.local' package/lean/default-settings/files/zzz-default-settings
+#sed -i '36a sed -i "3a /etc/init.d/led disable" /etc/rc.local' package/lean/default-settings/files/zzz-default-settings
 #sed -i '37a sed -i "4a /etc/init.d/shortcut-fe disable" /etc/rc.local' package/lean/default-settings/files/zzz-default-settings
 #sed -i '38a sed -i "5a wget https://cdn.jsdelivr.net/gh/yer2018/HomeLedeAutoBuild@master/AdGuardHome.yaml -O /etc/AdGuardHome/AdGuardHome.yaml" /etc/rc.local' package/lean/default-settings/files/zzz-default-settings
 #sed -i '39a sed -i "6a cp -f /etc/rc.local.bak /etc/rc.local" /etc/rc.local' package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0//" package/lean/default-settings/files/zzz-default-settings
+echo "sed -i s/openwrt.org/www.baidu.com/g /etc/config/luci" >> package/lean/default-settings/files/zzz-default-settings
+echo "sed -i '2a /etc/init.d/odhcpd disable' /etc/rc.local" >> package/lean/default-settings/files/zzz-default-settings
+echo "sed -i '3a /etc/init.d/led disable' /etc/rc.local" >> package/lean/default-settings/files/zzz-default-settings
+
+echo "exit 0" >> package/lean/default-settings/files/zzz-default-settings
